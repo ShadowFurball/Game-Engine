6b733c1de52bce215212c5286b1d4e87bac82d39
Tarkan Kemalzade <t.kemalzade@hotmail.co.uk>
2017-02-04 13:27:32 +0000

    Inital Engine Commit
    Intial github commit of the game engine

---
+++ Game-Engine/src/Engine-Main/engine-main.cpp
@@ -0,0 +1,18 @@
+/**
+	@file engine-main.cpp
+	@author Tarkan Kemalzade
+	@version 0.0.0.0
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	
+	std::cout << "Engine Name: Dark Nebula" << std::endl;
+	std::cout << "Engine Version: 0.0.0.0" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
